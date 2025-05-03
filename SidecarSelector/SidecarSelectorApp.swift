151c6ab62b560a3cbdd533af7dce787a22db7d52
koki <k.shiba30"gmail.com>
2025-05-03 16:57:10 +0900

    Initial Commit

---
+++ SidecarSelector/SidecarSelectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SidecarSelectorApp.swift
+//  SidecarSelector
+//
+//  Created by 柴田紘希 on 2025/05/03.
+//
+
+import SwiftUI
+
+@main
+struct SidecarSelectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SidecarSelectorTests/SidecarSelectorTests.swift
@@ -0,0 +1,17 @@
+//
+//  SidecarSelectorTests.swift
+//  SidecarSelectorTests
+//
+//  Created by 柴田紘希 on 2025/05/03.
+//
+
+import Testing
+@testable import SidecarSelector
+
+struct SidecarSelectorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
